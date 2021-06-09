9c5f8af60403834c3d91864d1eae7bc9b391e3ab
ahocine <ahocine@student.42.fr>
2021-06-09 03:02:24 +0200

    A

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,4 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putstr_fd(char *s, int fd)
+{
+	while (*s)
+		ft_putchar_fd(*s++, fd);
+}
