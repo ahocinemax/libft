1df162b047651b534eabe7450b18294cf2924940
Abdessalam HOCINE <ahocine@e3r5p8.42.fr>
2021-06-23 03:39:35 +0200

    a

--- libft.h
+++ libft.h
@@ -16,7 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
-# include <bsd/string.h>
+# include <string.h>
 
 void	*ft_memset(void *dest, int c, size_t len);
 void	ft_bzero(void *b, size_t len);
@@ -36,17 +36,17 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_strlcpy(char *dst, char *src, int n);						// KO
+size_t	ft_strlcpy(char *dst, const char *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strnstr(const char *big, const char	*little, size_t len);	// KO
-int		ft_atoi(const char *nb);										// KO
+char	*ft_strnstr(const char *big, const char	*little, size_t len);
+int		ft_atoi(const char *nb);
 void	*ft_calloc(size_t number, size_t len);
-char	*ft_strdup(const char *str);									// KO
-char	*ft_substr(char const *s, unsigned int start, size_t len);		// KO
+char	*ft_strdup(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);					// KO
 char	*ft_strtrim(char const *s1, char const *set);					// KO
 char	**ft_split(const char *str, char charset);						// KO
-char	*ft_itoa(int n);												// KO min value
+char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));		// KO
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);									// KO
