0497cf4501507d1af39d1ac2a5fa80d2d18c7b2f
Abdessalam HOCINE <ahocine@e1r12p19.42.fr>
2021-06-28 22:19:46 +0200

    A

--- libft.h
+++ libft.h
@@ -18,6 +18,23 @@
 # include <stdio.h>
 # include <string.h>
 
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+/*	Linked lists	*/
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **alst, t_list *new);
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **alst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+/*	Mandatory		*/
 void	*ft_memset(void *dest, int c, size_t len);
 void	ft_bzero(void *b, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t len);
