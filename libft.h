971a71dee24035317e175e22f22418bfeae99ca7
Abdessalam HOCINE <ahocine@e1r12p19.42.fr>
2021-06-28 21:21:08 +0200

    A

--- libft.h
+++ libft.h
@@ -25,7 +25,6 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t len);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 void	*ft_memchr(const void *b, int c, size_t len);
 int		ft_memcmp(const void *s1, const void *s2, size_t len);			// KO
-//    [fail]: your memcmp crash because it read too many bytes !
 int		ft_strlen(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -37,7 +36,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);			// KO
-//--> [fail]: your strncmp does not stop at \0
 size_t	ft_strlcpy(char *dst, const char *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *big, const char	*little, size_t len);
@@ -48,7 +46,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(const char *str, char charset);						// KO
-//--> [crash]: you did not protect your split
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
